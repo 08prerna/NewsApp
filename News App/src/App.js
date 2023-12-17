183ab8694cf9acb111610312ed879c51c81ab27b
Prerana Dhayfule <132763290+08prerna@users.noreply.github.com>
2023-12-17 17:57:59 +0530

    Add files via upload

--- News App/src/App.js
+++ News App/src/App.js
@@ -1,10 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
+import News from './News';
 function App() {
+
+  // let [articles,setArticles]=useState([]);
+
+  // useEffect(()=>{
+  //   fetch("https://newsapi.org/v2/everything?q=india&from=2023-12-16&apiKey=50adc61efba64530bec2771397767952")
+  //   .then((Response)=>Response.json())
+  //   .then((news)=>{
+  //     setArticles(news.articles);
+  //   })
+  //   .catch((err)=>{
+  //     console.log(err);
+  //   })
+  // },[])
+
+
   return (
     <div className="App">
-     <h1>Welcome to React</h1>
+
+      <header class="header">
+
+        <h1>News App</h1>
+        <input type='text' placeholder='Search News'/>
+      </header>
+      
+      <section class="news-articles">
+        <News/>
+        <News/>
+        <News/>
+        <News/>
+      </section>
+      
     </div>
   );
 }
