8f124d21248fba07fc8a4f11df57d8d081923e71
Prerana Dhayfule <132763290+08prerna@users.noreply.github.com>
2023-12-18 18:58:29 +0530

    Add files via upload

--- News App/src/News.js
+++ News App/src/News.js
@@ -1,7 +1,15 @@
 export default function News() {
     return(
         <div class="news">
+            <div class="news-img">
+                
+            </div>
             
+            <h1>News</h1>
+
+            <p>Random text will be here</p>
+
+            <a href="#">read more..</a>
         </div>
     )
 }
