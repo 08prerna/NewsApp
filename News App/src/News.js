6c5c2cff5e2f974ed39129b44f6bc5703d6c7517
Prerana Dhayfule <132763290+08prerna@users.noreply.github.com>
2023-12-20 18:45:32 +0530

    Add files via upload

--- News App/src/News.js
+++ News App/src/News.js
@@ -7,12 +7,12 @@ export default function News(props) {
             
             <h1>{props.article.title}</h1>
 
-            <p>{props.article.description?.substring(0,100).concat("....")}<a href={props.article.url} target="_blank">Read More</a></p>
+            <p>{props.article.description?.concat("....")}<a href={props.article.url} target="_blank">Read More</a></p>
 
         
             <div className="source">
-                <p>Author : {props.article.author}</p>
-                <p>{props.article.source.name}</p>
+                <p className="author">Author : {props.article.author}</p>
+                <p className="src-name">{props.article.source.name}</p>
             </div>
         </div>
     )
